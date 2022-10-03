94ab6a2b3fb7a540b9e3a84bc0f9c3dbf2ca4d04
Adithyan Ashok <adithyanashok406@gmail.com>
2022-10-03 12:07:22 +0530

    cors

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const connect = () => {
 // Middlewares
 app.use(cors({
     origin: "https://snazzy-twilight-089a42.netlify.app",
+    credentials: true
 }))
 app.use(cookieParser())
 app.use(express.json())
