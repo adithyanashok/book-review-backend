5c69bc99dee886797b96f53b8e565cd94e011c18
Adithyan Ashok <adithyanashok406@gmail.com>
2022-10-04 11:30:29 +0530

    cors

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -39,7 +39,6 @@ export const signIn = async (req, res) => {
 
         res.cookie("access_token", token, {
             httpOnly: true,
-            maxAge: 360000 + new Data(),
             
         }).status(200).json(others)
 
