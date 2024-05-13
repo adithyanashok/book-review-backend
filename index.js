a9367588c6ef2240bccd22d6529a60312d02c761
adithyanashok <72121693+adithyanashok@users.noreply.github.com>
2024-05-13 13:49:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ app.use((req, res, next) => {
 app.use(cors({
     origin : [
       'http://localhost:3000',
+      'https://book-app-eight-xi.vercel.app/',
       'https://book-app-ten-orpin.vercel.app'
     ],
     credentials: true,
